68a36050947f46391dbca4e6f9f403da189fe202
Anton Sapeha <antonsapega@gmail.com>
2022-07-14 13:00:58 +0300

    settings: there were added .env file with number of port, installed dotenv and uuid libs, primary configurations for start server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(port);
+  console.log(`🚀 The server is running on the port: ${port}`);
 }
 bootstrap();
